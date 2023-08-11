67cad6e929d589f5d622cc5f345f3bcdfc65f227
Saulnocap <ssaullcc@gmail.com>
2023-08-10 23:14:09 -0400

    Agregar archivos de proyecto.

---
+++ prueba/Controllers/InventariosController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using prueba.Models;
+
+namespace prueba.Controllers
+{
+    public class InventariosController : Controller
+    {
+        private readonly PruebaContext _context;
+
+        public InventariosController(PruebaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inventarios
+        public async Task<IActionResult> Index(string buscar)
+        {
+            var productos = from prueba in _context.Inventarios select prueba;
+
+            if (!String.IsNullOrEmpty(buscar))
+            {
+                productos = productos.Where(s => s.Nombre!.Contains(buscar));
+            }
+
+            return View(await productos.ToListAsync());
+        }
+
+        // GET: Inventarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Inventarios == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,ProdCode,Cantidad,Precio,Fecha,MovType,ProvCode")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inventario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Inventarios == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios.FindAsync(id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,ProdCode,Cantidad,Precio,Fecha,MovType,ProvCode")] Inventario inventario)
+        {
+            if (id != inventario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inventario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventarioExists(inventario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Inventarios == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Inventarios == null)
+            {
+                return Problem("Entity set 'PruebaContext.Inventarios'  is null.");
+            }
+            var inventario = await _context.Inventarios.FindAsync(id);
+            if (inventario != null)
+            {
+                _context.Inventarios.Remove(inventario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InventarioExists(int id)
+        {
+          return (_context.Inventarios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ prueba/Models/Proveedore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prueba.Models;
+
+public partial class Proveedore
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? ProvCode { get; set; }
+}
