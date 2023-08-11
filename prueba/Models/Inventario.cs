201842ee1370f364d5c0ef3865720d08b9ac2bfa
Saulnocap <ssaullcc@gmail.com>
2023-08-10 23:14:09 -0400

    Actualizado estilo del proyecto

---
+++ prueba/Models/Inventario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prueba.Models;
+
+public partial class Inventario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int? ProdCode { get; set; }
+
+    public int? Cantidad { get; set; }
+
+    public int? Precio { get; set; }
+
+    public DateTime? Fecha { get; set; }
+
+    public string? MovType { get; set; }
+
+    public int? ProvCode { get; set; }
+}
